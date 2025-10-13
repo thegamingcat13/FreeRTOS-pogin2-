0df13d674ec803a9b539ac0b7da03893bd8edef3
sparky170806 <ihme.denhartog@student.hu.nl>
2025-10-13 12:10:47 +0200

    comments bij tactile feedback

--- FreeRTOS_GPS_407/Core/MyApp/App/TactileFeedback.c
+++ FreeRTOS_GPS_407/Core/MyApp/App/TactileFeedback.c
@@ -15,6 +15,10 @@ float MAX_HEADING_DIFFERENCE = 10;
 int wpLat = 0;
 int wpLon = 0;
 
+/*
+ * de onderstaande functies worden gebruikt voor het aansturen van de L298N dual H-bridge driver.
+ * onderstaande functies kunnen worden opgeroepen om vooruit te rijden of te draaien.
+ */
 void turn_left()
 {
 	HAL_GPIO_WritePin(GPIOE, M1_1, SET);
@@ -38,6 +42,15 @@ void drive_foward()
 	HAL_GPIO_WritePin(GPIOE, M2_1, RESET);
 	HAL_GPIO_WritePin(GPIOE, M2_2, SET);
 }
+
+void stop()
+{
+	HAL_GPIO_WritePin(GPIOE, M1_1, RESET);
+	HAL_GPIO_WritePin(GPIOE, M1_2, RESET);
+	HAL_GPIO_WritePin(GPIOE, M2_1, RESET);
+	HAL_GPIO_WritePin(GPIOE, M2_2, RESET);
+}
+
 /*
 void spin_around()
 {
@@ -48,27 +61,24 @@ void spin_around()
 }
 */
 
-void stop()
-{
-	HAL_GPIO_WritePin(GPIOE, M1_1, RESET);
-	HAL_GPIO_WritePin(GPIOE, M1_2, RESET);
-	HAL_GPIO_WritePin(GPIOE, M2_1, RESET);
-	HAL_GPIO_WritePin(GPIOE, M2_2, RESET);
-}
 
+/*
+ * GoToDest wordt gebruikt om te bepalen of het voertuig al dicht genoeg bij de waypoint is om door te gaan.
+ * Indien het voertuig dicht genoeg erbij is gaan we naar de volgende waypoint.
+ */
 void GoToDest()
 {
-	if(fabs(parsed_gnrmc.latitude - wpLat) > 0.00002 && fabs(parsed_gnrmc.longitude - wpLon) > 0.00002)
-	{
+	if(fabs(parsed_gnrmc.latitude - wpLat) > LAT_PREC && fabs(parsed_gnrmc.longitude - wpLon) > LON_PREC)
+	{ // als de waarden groter zijn dan de define-waardes gaat het voertuig verder vooruit.
 		drive_foward();
 		osDelay(100);
 		xTaskNotifyGive(hParsedGPS);
 	}
-	else if(fabs(parsed_gnrmc.latitude - wpLat) < 0.00002 && fabs(parsed_gnrmc.longitude - wpLon) < 0.00002)
-	{
+	else if(fabs(parsed_gnrmc.latitude - wpLat) < LAT_PREC && fabs(parsed_gnrmc.longitude - wpLon) < LOT_PREC)
+	{ // als de waarden kleiner zijn dan de define-waardes gaan we door naar het volgende waypoint.
 		//spin_around();
-		stop();
-		CurrentWaypoint ++;
+		stop(); // waypoint bereikt, stop even
+		CurrentWaypoint ++; // ga door naar het volgende waypoint
 	}
 
 }
@@ -78,7 +88,7 @@ void ReachWPTask(void *argument)
 	ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
 	while (TRUE)
 	{
-		if (xSemaphoreTake(hGpsDataMutex, portMAX_DELAY) == pdTRUE)
+		if (xSemaphoreTake(hGpsDataMutex, portMAX_DELAY) == pdTRUE) // wacht tot de gps-mutex vrij is
 		{
 			while (wpLat == 0)
 			{
@@ -91,16 +101,17 @@ void ReachWPTask(void *argument)
 			{
 				wpLat = returnWaypoints(CurrentWaypoint, 1);
 				wpLon = returnWaypoints(CurrentWaypoint, 2);
-
+				// vergelijk de heading die aangehouden moet worden met de huidige heading
 				DesiredHeading = heading(CurrentWaypoint);
 				//CurrentHeading =
 
+				// als de headingwaarde kleiner is moet er naar rechts gedraaid worden
 				if(CurrentHeading > DesiredHeading)
 					turn_right();
-
+				// als de headingwaarde groter is moet er naar links gedraaid worden
 				else if(CurrentHeading < DesiredHeading)
 					turn_left();
-
+				// als het verschil in heading klein genoeg is kan er naar voren gereden worden en wordt GoToDest opgeroepen
 				else if(fabs(CurrentHeading - DesiredHeading) <  MAX_HEADING_DIFFERENCE)
 					GoToDest();
 			}
@@ -108,26 +119,39 @@ void ReachWPTask(void *argument)
 	}
 }
 
+/*
+ * SkipWaypoint wordt gebruikt om een waypoint over te slaan.
+ * Deze functie wordt aangeroepen door de ARM-keys.
+ * In de functie is een check ingebouwd die kijkt of de functie correct is uitgevoerd.
+ */
 int SkipWaypoint (void)
 {
 	int memory = CurrentWaypoint;
 	CurrentWaypoint++;
-	if (CurrentWaypoint == memory +1)
+	if (CurrentWaypoint == memory +1) // verhoog CurrentWaypoint met 1 om zo naar de volgende waypoint te gaan
 		return 1;
 	else
 		return 0;
 }
-
+/*
+ * BackWaypoint wordt gebruikt om terug te gaan naar de vorige waypoint.
+ * Deze functie wordt aangeroepen door de ARM-keys.
+ * In de functie is een check ingebouwd die kijkt of de functie correct is uitgevoerd.
+ */
 int BackWaypoint (void)
 {
 	int memory = CurrentWaypoint;
 	CurrentWaypoint--;
-	if (CurrentWaypoint == memory -1)
+	if (CurrentWaypoint == memory -1) // verlaag CurrentWaypoint met 1 om zo naar de vorige waypoint te gaan
 		return 1;
 	else
 		return 0;
 }
-
+/*
+ * ShowWaypoint wordt gebruikt om te laten zien welk waypoint we naartoe aan het gaan zijn en hoeveel waypoints er totaal zijn.
+ * Deze functie wordt aangeroepen door de ARM-keys.
+ * In de functie is een check ingebouwd die kijkt of de functie correct is uitgevoerd.
+ */
 int ShowWaypoint (void)
 {
 	int current = CurrentWaypoint;
